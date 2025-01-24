caf8b41c51f5f15c48d75fe0eecb842928e9f08a
Edward Ofori <tetraed.llc@gmail.com>
2025-01-23 21:58:24 -0500

    Added removed serving static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import path from 'node:path';
-import type { Request, Response } from 'express';
+// import path from 'node:path';
+// import type { Request, Response } from 'express';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
@@ -8,10 +8,10 @@ import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
 
 
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
 const server = new ApolloServer({
@@ -35,13 +35,13 @@ const startApolloServer = async () => {
     }
   ));
   
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  // if (process.env.NODE_ENV === 'production') {
+  //   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
+  //   app.get('*', (_req: Request, res: Response) => {
+  //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  //   });
+  // }
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
